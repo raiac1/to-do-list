4629942b6cf182f6b6b671675c9269accb52e99a
raiac1 <raiac1@apps.tcnj.edu>
2016-10-05 15:47:01 -0400

    Added Favorite color form

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'sqlite3'
+
 
 set :port, 8080
 set :static, true
@@ -13,9 +15,23 @@ get '/hello/' do
     erb :hello_form
 end
 
+#modify this for database
 post '/hello/' do
     greeting = params[:greeting] || "Hi There"
     name = params[:name] || "Nobody"
 
     erb :index, :locals => {'greeting' => greeting, 'name' => name}
+end
+get '/color/' do
+	erb :colorform
+end
+post '/color/' do
+	favorite = params[:favorite] || "Color"
+    name = params[:name] || "Nobody"
+    
+ db = SQLite3::Database.open "tester.db"
+	db.execute "CREATE TABLE IF NOT EXISTS colors (Id INTEGER PRIMARY KEY, 
+        Name TEXT, Favorite TEXT)"
+  	db.execute "INSERT INTO colors (Name, Favorite) values ('#{name}', '#{favorite}')"
+  	db.close
 end
