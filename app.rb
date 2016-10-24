e9c98221cf42c3d1b2d8ec944e7a7007152c6e25
raiac1 <raiac1@apps.tcnj.edu>
2016-10-24 13:36:09 -0400

    added delete route and lists items

--- app.rb
+++ app.rb
@@ -22,13 +22,35 @@ set :views, "views"
 
   #  erb :index, :locals => {'greeting' => greeting, 'name' => name}
 #end
-get '/color/' do
-	erb :colorform
-end
-post '/color/' do
-	favorite = params[:favorite] || "Color"
-    name = params[:name] || "Nobody"
 
+
+
+get '/:id/delete' do
+@note = params[:id]
+@title = "Confirm deletion of note ##{params[:id]}"
+erb :delete
+end
+
+delete '/:id' do
+ db = SQLite3::Database.open "tester.db"
+
+  #db.execute "CREATE TABLE IF NOT EXISTS colors (Id INTEGER PRIMARY KEY, 
+       # Name TEXT, Favorite TEXT)"
+
+ n = params[:id]
+ db.execute "delete from colors where id = '#{n}'"
+ return "Item deleted"
+end
+
+get '/color/' do
+  db = SQLite3::Database.open "tester.db"
+  @list = db.execute "select * from colors"
+  erb :colorform
+end
+
+post '/color/' do
+  favorite = params[:favorite] || "Color"
+    name = params[:name] || "Nobody"
 
 
  db = SQLite3::Database.open "tester.db"
@@ -36,14 +58,20 @@ post '/color/' do
 	db.execute "CREATE TABLE IF NOT EXISTS colors (Id INTEGER PRIMARY KEY, 
         Name TEXT, Favorite TEXT)"
 
+#where clause
+    #reads through names
     val= db.get_first_value "select name from colors where Name = '#{name}'"
+    #if name exists in database
   if val
-    puts "name already exists in database"
+    return "Name already exists in database"
+    #if name does not exist in database
     else
   	db.execute "INSERT INTO colors (Name, Favorite) values ('#{name}', '#{favorite}')"
   end
-
-  
-  	db.close
+db.close
 end
+
+#list = db.execute "*select* from colors"
+#list.each.do
+
 
